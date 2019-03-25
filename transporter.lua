8eac64b56bd494bcae39a4d278953efbc55cf6cd
Syte Beimin <sbeimin@betterbe.com>
2019-03-25 14:14:50 +0100

    initial crude transporter implementation

---
+++ transporter.lua
@@ -0,0 +1,26 @@
+local event = require("event") -- load event table and store the pointer to it in event
+local thread = require("thread")
+local ctifview = require("ctifview")
+
+function unknownEvent()
+  -- do nothing if the event wasn't relevant
+end
+
+continueLoop = true;
+
+local cleanup_thread = thread.create(function()
+  event.pull("interrupted")
+  continueLoop = false
+  print("Interrupt received. Exiting")
+  cleanup_thread()
+end)
+
+local main_thread = thread.create(function()
+  print("Waiting for incoming commands:")
+  while continueLoop do
+    local id, _, x, y = event.pullMultiple("touch", "interrupted")
+    if id == "touch" then
+      print("user clicked", x, y)
+    end
+  end
+end)
