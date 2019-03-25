96657579b26e1209363cc787e160d02978dc95ec
Syte Beimin <sbeimin@betterbe.com>
2019-03-25 15:13:56 +0100

    Set print color white.

--- transporter.lua
+++ transporter.lua
@@ -1,6 +1,8 @@
 local event = require("event") -- load event table and store the pointer to it in event
 local thread = require("thread")
 local ctifview = require("ctifview")
+local component = require("component")
+local gpu = component.gpu;
 
 function unknownEvent()
   -- do nothing if the event wasn't relevant
@@ -18,6 +20,7 @@ end)
 local main_thread = thread.create(function()
   ctifview.show('lib/TransporterDisplay_320.ctif')
   print("Waiting for incoming events:")
+  gpu.setForeground(0xFFFFFF)
   while continueLoop do
     local id, _, x, y = event.pullMultiple("touch", "interrupted")
     if id == "touch" then
